66aac7a149fa801390b096bbcb5c82d88342e5bb
DRussCSP <drussell28@students.stab.org>
2024-12-16 15:02:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ var year=getColumn(url,3);
 var month=getColumn(url,4);
 var day=getColumn(url,5);
 var price=getColumn(url,7)
+console.log(crypto)
 
-var avgPrice=[];
+var daySelect=document.getElementById("Day");
+for(var i=1; i<32; i++){
+    var el=document.createElement("option");
+    el.innerHTML=i;
+    el.value=i;
+    daySelect.appendChild(el);
+}
 
-for(var i=0; i<crypto.length; i++){
-    if((crypto[i]==document.getElementById("Crypto").value)&&(year[i]==document.getElementById("Year").value)&&(month[i]==document.getElementById("Month").value)){
-        avgPrice.push(price[i])
+
+
+
+
+function getPrice(cCrypto, yYear, mMonth, dDay){
+console.log(cCrypto);
+console.log(yYear);
+console.log(mMonth);
+console.log(dDay);
+var dayPrice=[];
+    for(var i=0; i<crypto.length; i++){
+        if((crypto[i]==cCrypto)&&(year[i]==yYear)&&(month[i]==mMonth)&&(parseInt(day[i])==dDay)){
+            dayPrice.push(price[i])
+        }
     }
+    console.log (dayPrice)
 }
